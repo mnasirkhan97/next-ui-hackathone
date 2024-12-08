004532186c2626f7c2d4b72ac00f6096ec3ee817
M Nasir Khan <immnk97@gmail.com>
2024-12-08 23:55:37 +0500

    app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import Blogs3 from "@/components/Blogs3";
+import Carousel2 from "@/components/Carousel2";
+import ContainerFluid from "@/components/ContainerFluid";
+import Hero from "@/components/Hero";
+import ProductCards10 from "@/components/ProductCards10";
+import ShopCard31 from "@/components/ShopCard31";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Quarter 2 Hackathon</div>;
+  return (
+    <div>
+      <Hero />
+      <ShopCard31 />
+      <ProductCards10 />
+      <Carousel2 />
+      <ContainerFluid />
+      <Blogs3 />
+    </div>
+  );
 }
