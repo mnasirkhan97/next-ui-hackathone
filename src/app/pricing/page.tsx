d222b64b406d5d4b9588d4c53412a83a28dac33f
M Nasir Khan <immnk97@gmail.com>
2024-12-16 23:49:08 +0500

    remove extra code

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -112,11 +112,3 @@ const PricingPage = () => {
 };
 
 export default PricingPage;
-
-// import React from "react";
-
-// function page() {
-//   return <div>Pricing page</div>;
-// }
-
-// export default page;
